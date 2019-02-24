32adeb32eab87aa236b5ebc58eb65a92aedea019
Adnan Hakim <adnanhakim007@gmail.com>
2019-02-24 13:56:27 +0530

    kaaafi zyada changes

--- app/src/main/java/com/adnanhakim/episode/TVSeries.java
+++ app/src/main/java/com/adnanhakim/episode/TVSeries.java
@@ -4,22 +4,12 @@ public class TVSeries {
 
     private int id;
     private String title, overview, imageURL;
-    private boolean favourite;
 
     public TVSeries(int id, String title, String overview, String imageURL) {
         this.id = id;
         this.title = title;
         this.overview = overview;
         this.imageURL = imageURL;
-        favourite = false;
-    }
-
-    public void setFavourite(boolean favourite) {
-        this.favourite = favourite;
-    }
-
-    public boolean isFavourite() {
-        return favourite;
     }
 
     public int getId() {

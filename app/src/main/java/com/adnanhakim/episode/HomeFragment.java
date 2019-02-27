23a32e4c4330505686b793295a467a96e6a2e69a
Arsh <arsh291206@gmail.com>
2019-02-27 18:18:01 +0530

    HomeFragment1

--- app/src/main/java/com/adnanhakim/episode/HomeFragment.java
+++ app/src/main/java/com/adnanhakim/episode/HomeFragment.java
@@ -114,7 +114,6 @@ public class HomeFragment extends Fragment {
                     Log.d(TAG, "onErrorResponse: Error: " + error.toString());
                 }
             });
-            Log.d(TAG,"Fucked Up");
             requestQueue.add(homeRequest);
         }
     }
